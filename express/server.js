d76c501a5dfaeefa346b1cda3adafb428d573630
ido sarue <dodojoker67@gmail.com>
2021-11-22 20:51:28 +0200

    learned how to create routes in express

--- express/server.js
+++ express/server.js
@@ -1,9 +1,16 @@
+// import "index.html"
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const path = require('path')
+
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('home')
+})
+
+app.get('/about', (req, res) => {
+  res.send('I\'m sss Ido')
 })
 
 app.listen(port, () => {
