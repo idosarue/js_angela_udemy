8a530f55a498f0b82a55f146654bda0804a7e5a1
ido sarue <dodojoker67@gmail.com>
2021-11-27 12:40:58 +0200

    section 19 done

--- express/calculator/calculator.js
+++ express/calculator/calculator.js
@@ -3,22 +3,44 @@ const express = require('express')
 const app = express()
 const port = 3000;
 const bodyParser = require("body-parser")
-const { body,validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-// body('name', 'Empty name').trim().isLength({ min: 1 }).escape(),
+app.use(express.json());
+app.post(
+  '/',
+  body('num1').isNumeric(),
+  body('num2').isNumeric(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+});
+
+
+exports.validate = (method) => {
+  switch (method) {
+    case 'createUser': {
+     return [ 
+        body('num1', 'not a num').isInt(),
+        body('num2', 'not a num').isInt(),
+       ]   
+    }
+  }
+}
 
 app.use(bodyParser.urlencoded({extended : true}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/static/index.html")
 })
 
-app.post('/', (req, res) => {
-    // res.send("thanks")
-    let num1 = parseInt(req.body.num1)
-    let num2 = parseInt(req.body.num2)
-    // body('num1', 'Invalid age').optional({ checkFalsy: true }).isNumeric(),
-    console.log(num1 + num2)
-  })
+// app.post('/', (req, res) => {
+//     // res.send("thanks")
+//     let num1 = parseInt(req.body.num1)
+//     let num2 = parseInt(req.body.num2)
+//     // body('num1', 'Invalid age').optional({ checkFalsy: true }).isNumeric(),
+//     console.log(num1 + num2)
+//   })
   
 
 app.listen(port, () => {
