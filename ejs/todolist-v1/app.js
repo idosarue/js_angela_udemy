cef939ddc63230285fc90ed4c1e0b540fa955f64
ido sarue <dodojoker67@gmail.com>
2021-11-29 12:12:35 +0200

    finished ejs section

--- ejs/todolist-v1/app.js
+++ ejs/todolist-v1/app.js
@@ -4,24 +4,29 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 const date = require(`${__dirname}/date.js`)
-
-
+const {check, validationResult} = require('express-validator')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.static('public'))
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 let listItems = []
 let workItems = []
 app.get('/', (req, res) => {
-    res.render('list', {listTitle: date(), tasks : listItems})
+    res.render('list', {listTitle: date.getDate(), tasks : listItems})
 })
 
-app.post('/' , (req, res) => {
+app.post('/' , urlencodedParser, [
+    check('task', 'Must be 3+ characters long')
+    .exists().isLength({min: 3})
+], (req, res) => {
     let task = req.body.task
-    console.log(req.body)
-    if (req.body.list === 'Work') {
-        workItems.push(task)
-        res.redirect('/work')
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        const alert = errors.array()
+        res.render('list', {
+            alert, listTitle : date.getDate(),  tasks : listItems
+        })
     }else {
         listItems.push(task)
         res.redirect('/')
@@ -29,8 +34,26 @@ app.post('/' , (req, res) => {
 
 })
 
+app.post('/work' , urlencodedParser, [
+    check('task', 'Must be 3+ characters long')
+    .exists().isLength({min: 3})
+], (req, res) => {
+    let task = req.body.task
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        const alert = errors.array()
+        res.render('work', {
+            alert, listTitle : 'Work List',  tasks : workItems
+        })
+    }else {
+        workItems.push(task)
+        res.redirect('/work')
+    }
+
+})
+
 app.get('/work', (req, res) => {
-    res.render('list', {listTitle : 'Work List',  tasks : workItems})
+    res.render('work', {listTitle : 'Work List',  tasks : workItems})
 })
 
 app.get('/about', (req, res) => {
--- ejs/todolist-v1/date.js
+++ ejs/todolist-v1/date.js
@@ -1,8 +1,12 @@
 
-module.exports = getDate
 
-function getDate() {
+module.exports.getDate = () => {
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let today = new Date()
     return today.toLocaleDateString("en-US", options)
+}
+
+module.exports.getDay = () =>  {
+    let today = new Date()
+    return today.toLocaleDateString("en-US", {weekday : 'long'})
 }
