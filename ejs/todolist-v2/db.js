c9bfa96730528bb183d03b60a68519403f01ef8e
ido sarue <dodojoker67@gmail.com>
2022-01-29 16:20:06 +0200

    merged

--- ejs/todolist-v2/db.js
+++ ejs/todolist-v2/db.js
@@ -1,27 +1,62 @@
 const mongoose = require('mongoose')
-
+const { Schema } = mongoose;
 mongoose.connect("mongodb://localhost:27017/toDoListDB")
 
 const itemsShema = mongoose.Schema({
     name : {
         type : String,
-        required : true
+        minLength : [5, 'The name must be at least 5 chars long'],
+        validate : {
+            validator : v => v.trim() != '',
+            message : props => 'The name must be at least 5 chars long'
+        }
+    },
+    list: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'listScehma'
     }
+
 })
 
 const Item = mongoose.model('Item', itemsShema)
 
-const item1 = new Item({
-    name : 's'
+const listSchema = mongoose.Schema({
+    name : {
+        type : String
+    },
+    items : [itemsShema]
 })
 
-const item2 = new Item({
-    name : 'THe second Item'
+
+
+const List = mongoose.model('List', listSchema)
+
+
+const list1 = new List({
+    name : 'Today',
 })
 
-const item3 = new Item({
-    name : 'THe third Item'
-})
 
-module.exports.defaultItems = [item1,item2,item3]
-module.exports.item = Item
+
+
+const createDefaultItems = async(list) => {
+    const defaultListName = ['The First Item', 'The Second Item', 'The Third Item']
+    const createdItems = []
+    for (let i = 0; i < defaultListName.length; i++) {
+        console.log(i)
+        const newItem = new Item({
+            name : defaultListName[i],
+            list : list
+        })
+        newItem.save()
+        createdItems.push(newItem)
+    }
+
+    list.items = createdItems
+    list.save()
+}
+
+module.exports.Item = Item
+module.exports.List = List
+module.exports.createDefaultItems = createDefaultItems
+module.exports.firstList = list1
+
